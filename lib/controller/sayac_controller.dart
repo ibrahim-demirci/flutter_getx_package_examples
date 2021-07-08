26e539c5e3e3678d01b6f90ffbfdb5fca0c452ee
ibrahim-demirci <ibrahim.demirci@marun.edu.tr>
2021-07-08 22:49:11 +0300

    count controller class added

---
+++ lib/controller/sayac_controller.dart
@@ -0,0 +1,19 @@
+import 'package:get/get.dart';
+
+class SayacController extends GetxController {
+  // This value have .obs beacuse this can change.
+  var _count = 0.obs;
+
+  int get count => _count.value;
+  set count(int value) => _count.value = value;
+
+  void increase() {
+    // Set and get using the blow.
+    count = count + 1;
+  }
+
+  void decrease() {
+    // Set and get using the blow.
+    count = count - 1;
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -5,16 +5,54 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Material App Bar'),
-        ),
-        body: Center(
-          child: Container(
-            child: Text('Hello World'),
+    return MaterialApp(title: 'Material App', home: MyHomePage());
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            heroTag: '1',
+            onPressed: () {},
+            child: Icon(Icons.add),
           ),
+          FloatingActionButton(
+            heroTag: '2',
+            onPressed: () {},
+            child: Icon(Icons.remove),
+          ),
+          FloatingActionButton(
+            heroTag: '3',
+            onPressed: () {},
+            child: Icon(Icons.add),
+          ),
+          FloatingActionButton(
+            heroTag: '4',
+            onPressed: () {},
+            child: Icon(Icons.add),
+          ),
+        ],
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Merhaba',
+              style: TextStyle(fontSize: 32),
+            ),
+            Text(
+              'Selam',
+              style: TextStyle(fontSize: 32),
+            )
+          ],
         ),
       ),
     );
