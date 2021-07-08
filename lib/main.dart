ce5356f4dc0433d4ac378cb2961204147d0624e2
ibrahim-demirci <ibrahim.demirci@marun.edu.tr>
2021-07-08 23:05:27 +0300

    added GetX<CounterController> and Obx in another widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:getx_package/controller/counter_controller.dart';
-import 'package:getxfire/getxfire.dart';
+import 'package:get/get.dart';
 
 void main() => runApp(MyApp());
 
@@ -51,23 +51,38 @@ class MyHomePage extends StatelessWidget {
         ],
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            // Obx for the text widget can listen the changes.
-            Obx(
-              () => Text(
-                ' ${_controller.count} times you click',
-                style: TextStyle(fontSize: 32),
-              ),
-            ),
-            Text(
-              'Selam',
-              style: TextStyle(fontSize: 32),
-            )
-          ],
-        ),
+        child: MyColumn(),
       ),
+    );
+  }
+}
+
+class MyColumn extends StatelessWidget {
+  // This code below find the Controller on widget tree.
+  CounterController _findController = Get.find();
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        GetX<CounterController>(
+          builder: (_controller2) => Text(
+            ' ${_controller2.count} times you click',
+            style: TextStyle(fontSize: 32),
+          ),
+        ),
+        Obx(
+          () => Text(
+            ' ${_findController.count} times you click',
+            style: TextStyle(fontSize: 32),
+          ),
+        ),
+        Text(
+          'Selam',
+          style: TextStyle(fontSize: 32),
+        )
+      ],
     );
   }
 }
